1b866eb6d74c1a0a68667ec14af0eebd834388c8
dika maulana <dikaxmultimedia@gmail.com>
2022-10-17 15:10:37 +0700

    upload pertama

---
+++ application/controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+// Controller
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('home');
+    }
+
+    public function tampil()
+    {
+        return $this->load->view('v_tampil');
+    }
+
+    public function coba()
+    {
+        return $this->load->view('test');
+        
+    }
+}
+
+/* End of file Home.php */
---
+++ application/controllers/web.php
@@ -0,0 +1,11 @@
+<?php
+// Controller
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class web extends CI_Controller
+{
+    public function lanjut()
+	{
+		echo "WEB Framework";
+	}
+}
